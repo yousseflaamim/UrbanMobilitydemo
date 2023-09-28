f359412e41c6ba915dfad8decca7b47929162f98
yousseflaamim <39153351+yousseflaamim@users.noreply.github.com>
2023-09-28 02:01:20 +0200

    Update UserTest.java

--- ubertrafik/demo/src/test/java/com/example/demo/UserTest.java
+++ ubertrafik/demo/src/test/java/com/example/demo/UserTest.java
@@ -1,24 +1,2 @@
 package com.example.demo;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import com.example.demo.modell.User;
-import com.example.demo.service.UserService;
-import org.junit.jupiter.api.Test;
-
-public class UserTest {
-
-    @Test
-    public void testCreateUserAccount() {
-        UserService userService = new UserService();
-
-
-        User user = userService.createUserAccount("testUser", "testContactInfo","","", User.AccountType.user);
-
-
-        assertNotNull(user);
-        assertEquals("testUser", user.getUsername());
-        assertEquals("testContactInfo", user.getContactInfo());
-    }
-}
-
