2de90dff26572abc3286d5554afaac6bb775c795
yousseflaamim <39153351+yousseflaamim@users.noreply.github.com>
2023-09-28 02:02:33 +0200

    Update ProviderTest.java

--- ubertrafik/demo/src/test/java/com/example/demo/ProviderTest.java
+++ ubertrafik/demo/src/test/java/com/example/demo/ProviderTest.java
@@ -1,22 +1,3 @@
 package com.example.demo;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-import com.example.demo.service.ProviderService;
-import org.junit.jupiter.api.Test;
-
-public class ProviderTest {
-
-    @Test
-    public void testChooseTransportation() {
-        // إعداد مزيف للخدمة
-        ProviderService providerService = new ProviderService();
-
-        // اختبار اختيار وسيلة النقل
-        String transportationType = providerService.chooseTransportation(null, "car");
-
-        // التحقق من نوع وسيلة النقل المختار
-        assertNotNull(transportationType);
-        assertEquals("Car", transportationType);
-    }
-}
