7d5a286d7a81878faa8b550fb129ec546ecc32f1
yousseflaamim <39153351+yousseflaamim@users.noreply.github.com>
2023-10-02 00:35:15 +0200

    Update UserService.java

--- ubertrafik/demo/src/main/java/com/example/demo/service/UserService.java
+++ ubertrafik/demo/src/main/java/com/example/demo/service/UserService.java
@@ -28,7 +28,6 @@ public class UserService {
     @Autowired
     private PaymentService paymentService;
 
-    // أنشطة إنشاء حساب المستخدم
     public User createUserAccount(String username, String contactInfo, String paymentInfo,
                                   String paymentDate, User.AccountType accountType) {
         User account = new User();
@@ -40,29 +39,24 @@ public class UserService {
         return userRepository.save(account);
     }
 
-    // أنشطة إنشاء حساب مقدم الخدمة
 
 
 
 
     public User findUserByUsername(String username) {
         return (User) userRepository.findByUsername(username)
-                .orElseThrow(() -> new EntityNotFoundException("مستخدم غير موجود بالاسم: " + username));
+                .orElseThrow(() -> new EntityNotFoundException("User not found by name: " + username));
     }
 
-    // البحث عن مقدم خدمة بالاسم
 
 
-    // حذف مستخدم بالاسم
     public void deleteUserByUsername(String username) {
         User user = findUserByUsername(username);
         userRepository.delete(user);
     }
 
-    // حذف مقدم خدمة بالاسم
 
 
-    // تحديث معلومات المستخدم
     public User updateUser(String username, String contactInfo, String paymentInfo) {
         User user = findUserByUsername(username);
         user.setContactInfo(contactInfo);
@@ -70,7 +64,6 @@ public class UserService {
         return userRepository.save(user);
     }
 
-    // تحديث معلومات مقدم الخدمة
 
 
     public List<User> getAllUsers() {
