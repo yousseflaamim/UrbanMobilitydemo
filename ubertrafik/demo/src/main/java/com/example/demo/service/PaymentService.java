594fddc235d905c99d7fd090a7dc9402d54b7f4e
yousseflaamim <39153351+yousseflaamim@users.noreply.github.com>
2023-10-02 00:32:11 +0200

    Update PaymentService.java

--- ubertrafik/demo/src/main/java/com/example/demo/service/PaymentService.java
+++ ubertrafik/demo/src/main/java/com/example/demo/service/PaymentService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class PaymentService {
 
-    // يمكنك حقن واجهة المستودع (repository) هنا لتخزين معلومات الدفع في قاعدة البيانات
     @Autowired
     private  PaymentRepository paymentRepository;
 
@@ -22,25 +21,18 @@ public class PaymentService {
     }
 
     public boolean processPayment(User user, Booking booking) {
-        // قم بتنفيذ عملية الدفع هنا
-        // يمكنك استخدام خدمات الدفع الخارجية مثل Stripe أو PayPal أو غيرها
-
-        // في هذا المثال، سنفترض أن عملية الدفع ناجحة دائمًا
-        // يمكنك تغيير هذا وفقًا للتكامل مع خدمة الدفع الفعلية
+      
         boolean paymentSuccess = true;
 
         if (paymentSuccess) {
-            // إذا كانت عملية الدفع ناجحة، قم بتحديث حالة الحجز إلى مدفوع
             booking.setPaid(true);
 
-            // يمكنك أيضًا حفظ تفاصيل عملية الدفع في قاعدة البيانات إذا كنت بحاجة إلى ذلك
             Payment payment = new Payment();
             payment.setBooking(booking);
             payment.setAccount(user);;
             payment.setAmount(booking.getFare());
 
-            // حفظ تفاصيل الدفع في قاعدة البيانات
-            paymentRepository.save(payment);
+                paymentRepository.save(payment);
         }
 
         return paymentSuccess;
