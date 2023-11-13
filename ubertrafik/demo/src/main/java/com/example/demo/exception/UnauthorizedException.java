00095ce488ced7b83c81c0a0dd3f2d00c19ae297
yousseflaamim <39153351+yousseflaamim@users.noreply.github.com>
2023-11-13 21:25:31 +0100

    Delete ubertrafik/demo directory

--- ubertrafik/demo/src/main/java/com/example/demo/exception/UnauthorizedException.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.exception;
-
-public class UnauthorizedException extends Throwable {
-    public UnauthorizedException(String message ) {super(message);
-    }
-}
--- ubertrafik/demo/src/main/java/com/example/demo/repostory/BookingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repostory;
-
-import com.example.demo.modell.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-}
